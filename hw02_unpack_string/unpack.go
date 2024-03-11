d27a469a730b17948e8dc99888ed00f132b447c2
a.v.romanov <a.v.romanov@vk.team>
2024-03-11 21:07:39 +0300

    hw2: fix after review

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -23,20 +23,19 @@ func Unpack(input string) (string, error) {
 
 	var result strings.Builder
 
-	atoi := func(str string) int {
-		val, _ := strconv.Atoi(str)
-		return val
-	}
-
 	var repData string
 
 	for i, v := range input {
 		if unicode.IsLetter(v) {
 			switch {
 			case !unicode.IsDigit(str[i+1]):
-				repData = strings.Repeat(string(v), 1)
+				repData = string(v)
 			case unicode.IsDigit(str[i+1]) && !unicode.IsDigit(str[i+2]):
-				repData = strings.Repeat(string(v), atoi(string(str[i+1])))
+				val, err := strconv.Atoi(string(str[i+1]))
+				if err != nil {
+					return "", err
+				}
+				repData = strings.Repeat(string(v), val)
 			default:
 				return "", ErrInvalidString
 			}
